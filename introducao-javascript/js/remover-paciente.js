426a37f536472d7127b8087a90f3d7196b9dd798
Carla Dias <carllottadias@gmail.com>
2020-08-19 17:37:57 -0300

    Adcionando animação na remoção de elementos da tabela

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -3,10 +3,17 @@ var pacientes = document.querySelectorAll(".paciente");
 var table = document.querySelector("table")
 
 table.addEventListener("dblclick", function(event){
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode;
 
-    paiDoAlvo.remove();
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },5000);
+
+    // var alvoEvento = event.target;
+    // var paiDoAlvo = alvoEvento.parentNode;
+
+    // paiDoAlvo.remove();
 
     //event.target.parentNode.remove();
 
